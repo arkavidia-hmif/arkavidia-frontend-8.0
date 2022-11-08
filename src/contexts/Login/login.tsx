eca57b4450e957553e28d36c59176fa123f99f95
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-11-08 20:45:39 +0700

    refactor: minor refactoring

--- src/contexts/Login/login.tsx
+++ src/contexts/Login/login.tsx
@@ -2,14 +2,17 @@ import CustomButton from '@src/components/CustomButton/CustomButton'
 import Layout from '@src/components/Navigation/Layout'
 import { TextField } from '@src/components/TextField'
 
-const Login: React.FC<any> = () => {
+interface ILoginContext {}
+const Login: React.FC<ILoginContext> = () => {
   return (
     <Layout title="Sign In" description="Sign In">
       <div className="flex flex-row justify-between w-full h-screen">
         {/* Sisi Kiri */}
         <div className="container hidden md:flex flex-col justify-center items-center gap-[3.75rem] w-1/2 bg-yellow300">
           {/* Image Container */}
-          <div className="h-[480px] w-[400px] bg-blue200">Gaada logonya :(</div>
+          <div className="h-[360px] w-[300px] lg:h-[480px] lg:w-[400px] bg-blue200">
+            Gaada logonya :(
+          </div>
 
           {/* Text */}
           <div className="flex flex-col gap-4">
@@ -29,10 +32,10 @@ const Login: React.FC<any> = () => {
           <div className="h-[167px] w-[167px] bg-blue200">Gaada logonya :(</div>
           {/* Text Container */}
           <div className="flex flex-col justify-center items-center">
-            <h1 className="z-10 font-archivo text-2xl md:text-4xl text-center md:w-[200px] font-black text-white py-1 px-3 md:py-2 md:px-10 bg-black rotate-[7deg] -mb-1">
+            <h1 className="z-10 font-archivo text-2xl md:text-4xl text-center md:w-[200px] font-black text-white py-1 px-6 md:py-2 md:px-10 bg-black rotate-[7deg] -mb-[0.125rem]">
               Hello
             </h1>
-            <h1 className="z-0 font-archivo text-2xl md:text-4xl text-center md:w-[336px] font-black text-white py-1 px-3 md:py-2 md:px-6 bg-black">
+            <h1 className="z-0 font-archivo text-2xl md:text-4xl text-center md:w-[336px] font-black text-white py-1 px-6 md:py-2 md:px-6 bg-black">
               Arkavidians!
             </h1>
           </div>
