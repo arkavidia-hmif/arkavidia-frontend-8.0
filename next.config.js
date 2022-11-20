84c5d2c39864fdb6d13bda9e1af0fe593f68c615
reyshazni013 <18220013@mahasiswa.itb.ac.id>
2022-11-21 00:18:48 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -12,11 +12,6 @@ module.exports = {
     },
   },
   swcMinify: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
