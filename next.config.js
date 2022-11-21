19008bdb5fdd72eed6d471335bf40b4038df79be
modanism <18220087@std.stei.itb.ac.id>
2022-11-21 14:20:29 +0700

    ignore eslint and remove unused import in submission

--- next.config.js
+++ next.config.js
@@ -36,4 +36,9 @@ module.exports = {
     loader: 'akamai',
     path: '',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
