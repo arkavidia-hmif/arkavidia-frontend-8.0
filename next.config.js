67665234342113ca22527f66a312f832b09a0ea8
reyshazni013 <18220013@mahasiswa.itb.ac.id>
2022-11-21 21:37:13 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -12,22 +12,6 @@ module.exports = {
     },
   },
   swcMinify: true,
-  exportPathMap: async function (defaultPathMap) {
-    const pathMap = {};
-
-    for (const [path, config] of Object.entries(defaultPathMap)) {
-      if (path === "/") {
-        pathMap[path] = config;
-      } else {
-        pathMap[`${path}/index`] = config;
-      }
-    }
-
-    return pathMap;
-  },
-  images: {
-    loader: 'akamai',
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
