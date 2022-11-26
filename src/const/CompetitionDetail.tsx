e189c7053acfcef4f1ed2e9b06c1dab71d1149d7
reyshazni <reyshazni@gmail.com>
2022-11-27 01:19:32 +0700

    feat: add timeline detail competition

---
+++ src/const/CompetitionDetail.tsx
@@ -0,0 +1,179 @@
+export const TIMELINE_ARKALOGICA = [
+    {
+      title: "Registration",
+      startDate: new Date("2022/11/29"),
+      endDate: new Date("2022/12/24")
+    },
+    {
+      title: "Close Registration",
+      startDate: new Date("2022/12/24"),
+      endDate: new Date("2022/12/31")
+    },
+    {
+        title: "Warmup Penyisihan",
+        startDate: new Date("2023/01/20"),
+        endDate: new Date("2023/01/21")
+    },
+    {
+        title: "Penyisihan",
+        startDate: new Date("2023/01/21"),
+        endDate: new Date("2023/01/22")
+    },
+    {
+        title: "Pengumuman Finalist",
+        startDate: new Date("2023/02/04"),
+        endDate: new Date("2023/02/05")
+    },
+    {
+        title: "Warm Up Final",
+        startDate: new Date("2023/02/24"),
+        endDate: new Date("2023/02/25")
+    },
+    {
+        title: "Final",
+        startDate: new Date("2023/02/25"),
+        endDate: new Date("2023/02/26")
+    },
+  ]
+  
+export const FAQ_ARKALOGICA = [
+    {
+      title: "Apa Itu Arkalogica?",
+      content: "Arkalogica adalah kompetisi yang mengasah kemampuan dalam penggunaan logika dan menyelesaikan masalah secara logis. Jenis-jenis permasalahan yang diberikan antara lain berbagai soal logika dan game menarik"
+    }
+  ]
+
+export const TIMELINE_CP = [
+    {
+      title: "Registration",
+      startDate: new Date("2022/11/29"),
+      endDate: new Date("2022/12/24")
+    },
+    {
+      title: "Close Registration",
+      startDate: new Date("2022/12/24"),
+      endDate: new Date("2022/12/31")
+    },
+    {
+        title: "Warmup Penyisihan",
+        startDate: new Date("2023/01/20"),
+        endDate: new Date("2023/01/21")
+    },
+    {
+        title: "Penyisihan",
+        startDate: new Date("2023/01/21"),
+        endDate: new Date("2023/01/22")
+    },
+    {
+        title: "Pengumuman Finalist",
+        startDate: new Date("2023/02/04"),
+        endDate: new Date("2023/02/05")
+    },
+    {
+        title: "Warm Up Final",
+        startDate: new Date("2023/02/24"),
+        endDate: new Date("2023/02/25")
+    },
+    {
+        title: "Final",
+        startDate: new Date("2023/02/25"),
+        endDate: new Date("2023/02/26")
+    },
+  ]
+  
+export const FAQ_CP = [
+    {
+      title: "Apa Itu Arkalogica?",
+      content: "Arkalogica adalah kompetisi yang mengasah kemampuan dalam penggunaan logika dan menyelesaikan masalah secara logis. Jenis-jenis permasalahan yang diberikan antara lain berbagai soal logika dan game menarik"
+    }
+  ]
+
+export const TIMELINE_DATAVIDIA = [
+    {
+      title: "Registration",
+      startDate: new Date("2022/11/29"),
+      endDate: new Date("2022/12/24")
+    },
+    {
+      title: "Close Registration",
+      startDate: new Date("2022/12/24"),
+      endDate: new Date("2022/12/31")
+    },
+    {
+        title: "Penyisihan",
+        startDate: new Date("2023/01/7"),
+        endDate: new Date("2023/01/21")
+    },
+    {
+        title: "Pengumuman Finalist",
+        startDate: new Date("2023/02/04"),
+        endDate: new Date("2023/02/05")
+    },
+    {
+        title: "Submisi Pra-Final",
+        startDate: new Date("2023/02/05"),
+        endDate: new Date("2023/02/20")
+    },
+    {
+        title: "Warm Up Final",
+        startDate: new Date("2023/02/24"),
+        endDate: new Date("2023/02/25")
+    },
+    {
+        title: "Final",
+        startDate: new Date("2023/02/25"),
+        endDate: new Date("2023/02/26")
+    },
+  ]
+  
+export const FAQ_DATAVIDIA = [
+    {
+      title: "Apa Itu Arkalogica?",
+      content: "Arkalogica adalah kompetisi yang mengasah kemampuan dalam penggunaan logika dan menyelesaikan masalah secara logis. Jenis-jenis permasalahan yang diberikan antara lain berbagai soal logika dan game menarik"
+    }
+  ]
+
+  export const TIMELINE_UXVIDIA = [
+    {
+      title: "Registration",
+      startDate: new Date("2022/11/29"),
+      endDate: new Date("2022/12/24")
+    },
+    {
+      title: "Close Registration",
+      startDate: new Date("2022/12/24"),
+      endDate: new Date("2022/12/31")
+    },
+    {
+        title: "Penyisihan",
+        startDate: new Date("2023/01/7"),
+        endDate: new Date("2023/01/21")
+    },
+    {
+        title: "Pengumuman Finalist",
+        startDate: new Date("2023/02/04"),
+        endDate: new Date("2023/02/05")
+    },
+    {
+        title: "Submisi Pra-Final",
+        startDate: new Date("2023/02/05"),
+        endDate: new Date("2023/02/20")
+    },
+    {
+        title: "Warm Up Final",
+        startDate: new Date("2023/02/24"),
+        endDate: new Date("2023/02/25")
+    },
+    {
+        title: "Final",
+        startDate: new Date("2023/02/25"),
+        endDate: new Date("2023/02/26")
+    },
+  ]
+  
+export const FAQ_UXVIDIA = [
+    {
+      title: "Apa Itu Arkalogica?",
+      content: "Arkalogica adalah kompetisi yang mengasah kemampuan dalam penggunaan logika dan menyelesaikan masalah secara logis. Jenis-jenis permasalahan yang diberikan antara lain berbagai soal logika dan game menarik"
+    }
+  ]
