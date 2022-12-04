bebd6534cd02ea7988bfe744deb008665d69b0a9
fernaldifz <fernaldifz@gmail.com>
2022-12-04 16:38:04 +0700

    update homepage faq styling

--- src/contexts/Homepage/FAQ.tsx
+++ src/contexts/Homepage/FAQ.tsx
@@ -4,7 +4,7 @@ import FAQItem from "./FAQItem";
 export default function FAQ() {
     return (
         <>
-            <div className="container mx-auto py-2 lg:max-w-4xl md:max-w-2xl sm:max-w-lg">
+            <div className="container mx-auto py-2 lg:max-w-4xl md:max-w-2xl sm:max-w-lg max-w-xs">
                 <h1 className="text-center text-[60px] font-archivo font-[900] mb-[60px]">F.A.Q.</h1>
                 {faqs.map(item => (
                     <FAQItem
--- src/contexts/Homepage/FAQItem.tsx
+++ src/contexts/Homepage/FAQItem.tsx
@@ -3,15 +3,15 @@ import ArrowDownIcon from "@src/components/Icon/ArrowDownIcon";
 // import ArrowTopIcon from "@src/components/Icon/ArrowTopIcon";
 
 type faqsProps = {
-  question: string
-  answer: string | JSX.Element
+    question: string
+    answer: string | JSX.Element
 }
 
 const FAQItem = ({ question, answer }: faqsProps) => {
     const [isActive, setIsActive] = useState(false);
 
     return (
-        <div className="py-1">
+        <div className="py-4">
             <div className={`
                 border
                 border-black
@@ -24,19 +24,19 @@ const FAQItem = ({ question, answer }: faqsProps) => {
                 text-white text-left text-xl
                 bg-black
                 transition
-                cursor-pointer ${isActive ? 'rounded-t-lg' : 'rounded-lg'}`} 
+                cursor-pointer ${isActive ? 'rounded-t-lg' : 'rounded-lg'}`}
                 onClick={() => setIsActive(!isActive)}
             >
                 <div className="font-['Varela-Round']">{question}</div>
                 {/* With Animation */}
-                <div>{<ArrowDownIcon 
-                        size={28}
-                        className={`absolute bottom-1/4 right-0 mr-4 cursor-pointer ${isActive ? 'transition-transform rotate-180 mr-3.5' : 'transition-transform rotate-0'}`}
-                        color="#f9f9f9"
-                    />}
+                <div>{<ArrowDownIcon
+                    size={28}
+                    className={`absolute bottom-1/4 right-0 mr-4 cursor-pointer ${isActive ? 'transition-transform rotate-180 mr-3.5' : 'transition-transform rotate-0'}`}
+                    color="#f9f9f9"
+                />}
                 </div>
             </div>
-            {isActive && 
+            {isActive &&
                 <div className="bg-white py-3 px-5 border-x-2 border-b-2 border-black rounded-b-lg">
                     {answer}
                 </div>
