6305545c25c523e913c747cc1e32e61e26db760f
Fatih20 <fnri39@protonmail.com>
2022-12-18 07:00:51 +0700

    Add empty announcement page.

--- src/contexts/CMS/Pengumuman/index.tsx
+++ src/contexts/CMS/Pengumuman/index.tsx
@@ -2,7 +2,8 @@ import TabelPengumuman, {
   PengumumanFetched
 } from '@src/components/CMS/Pengumuman/Table'
 import CustomButton from '@src/components/CustomButton/CustomButton'
-import { PengumumanState } from '@src/utils/customHooks/pengumuman'
+import EmptyAnnouncement from '@src/assets/images/EmptyAnnouncement.svg'
+import Image from 'next/image'
 
 function CMSPengumuman({
   pengumumanList
@@ -18,7 +19,8 @@ function CMSPengumuman({
         </CustomButton>
       </div>
       {pengumumanList.length <= 0 ? (
-        <div className="text-center flex flex-col items-center justify-start flex-grow">
+        <div className="text-center flex flex-col items-center justify-start flex-grow gap-6">
+          <Image src={EmptyAnnouncement} />
           <p className="font-varela text-xl">
             Belum Ada Pengumuman Nih. <br />
             Tetap Stay Tune Ya!
