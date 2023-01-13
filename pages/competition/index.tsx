7a5bbb8883e837ea909f4c3e7d228c083d0c7066
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-01-13 22:52:12 +0700

    feat: landing event coming soon web version

--- pages/competition/index.tsx
+++ pages/competition/index.tsx
@@ -1,16 +1,18 @@
-import SEO from '@src/components/SEO';
-import Jumbotron from '@src/contexts/Competition/Jumbotron';
+import SEO from '@src/components/SEO'
+import Jumbotron from '@src/contexts/Competition/Jumbotron'
 import Prize from '@src/contexts/Competition/Prize'
 import Category from '@src/contexts/Competition/Category'
+import EventComingSoon from '@src/contexts/EventLanding/EventComingSoon'
 
 const Competition = (): JSX.Element => {
   return (
     <SEO title="Arkavidia 8.0 | Competition">
       <Jumbotron />
-      <Prize/>
-      <Category/>
+      <EventComingSoon />
+      <Prize />
+      <Category />
     </SEO>
   )
 }
 
-export default Competition;
+export default Competition
---
+++ src/contexts/EventLanding/EventComingSoon.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import comingSoon from '@src/assets/images/event-landing/comingsoon.svg'
+import boy from '@src/assets/images/event-landing/boy.svg'
+
+const EventComingSoon = () => {
+  return (
+    <div className="relative flex flex-col items-center w-full h-full ">
+      <div className="w-[841px] h-[104px] bg-black flex justify-center items-center">
+        <h1 className="font-archivo font-black text-white text-[72px]">
+          MAIN EVENT
+        </h1>
+      </div>
+
+      <div className="w-full flex justify-center items-center mt-[100px] pb-[150px]">
+        <Image src={comingSoon} />
+        <div className="absolute top-28 right-0">
+          <Image src={boy} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EventComingSoon
