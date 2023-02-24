c21204ce19d940d28f503e43767f1298a1341e16
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2023-02-24 10:32:42 +0700

    fix: itfest redirect

--- pages/event/itfest/index.tsx
+++ pages/event/itfest/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import SEO from '@src/components/SEO';
 import ComingSoon from '@src/contexts/ComingSoon/ComingSoon';
 
 const Home = (): JSX.Element => {
+  useEffect(() => {
+    window.location.href = 'https://itfest.arkavidia.com/u/home';
+  }, [])
+
   return (
     <SEO title="Arkavidia 8.0">
       <ComingSoon />
